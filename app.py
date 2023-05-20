acb99a763db109c10d2f58cd808ac80f3ad5f0cc
hohooing <130472968+hohooing@users.noreply.github.com>
2023-05-20 21:56:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
+import pymongo
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('./index.html')
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = client['mydatabase']
+collection = db['codes']
+
+@app.route('/code', methods=['GET'])
+def get_code():
+    latest_code = collection.find_one(sort=[("date", pymongo.DESCENDING)])
+    return render_template('index.html',code=latest_code['code'])
+if __name__ == '__main__':
+    app.run()
+
